db490b2c2e76f7fe0e81af9e43ecb6263857b3ec
Ed Perkin <edperkin@outlook.com>
2024-10-26 15:53:00 +0100

    - getting and using produce data from api

--- src/app/produce/produce.component.ts
+++ src/app/produce/produce.component.ts
@@ -20,17 +20,19 @@ export class ProduceComponent implements OnInit {
   produceTypes: string[] = ['Fruit', 'Vegetables'];
   selectedProduceType: string = '';
   selectedProduceTypeIndex: number = 0;
-  produceThisWeek: string[] | null = null;
+  produceThisWeek: any | null = null;
   dropdownOpen: boolean = false;
   date: string = '';
   errorMessage: string | undefined;
+  produce: string = '';
+  imageUrl: string = '';
+  weeksRemaining: string = '';
+  fontColour: string = '#ffffff';
 
   constructor(private route: ActivatedRoute, private router: Router, private produceService: ProduceService) {
   }
 
   ngOnInit(): void {
-    this.produceType = this.route.snapshot.paramMap.get('id');
-
     this.route.paramMap.subscribe(params => {
       const id = params.get('id');
 
@@ -39,26 +41,37 @@ export class ProduceComponent implements OnInit {
       } else {
         this.selectedProduceType = this.produceTypes[0];
       }
+
+      this.selectedProduceTypeIndex = this.produceTypes.indexOf(this.selectedProduceType);
+
+      this.fetchProduceItems();
     });
 
-    this.selectedProduceTypeIndex = this.produceTypes.indexOf(this.selectedProduceType);
-
     this.date = this.getDate();
+  }
 
+  fetchProduceItems(): void {
     const date = new Date();
-    //*********
     const week = this.getWeekNumber(date);
+
     this.produceService.getItemsByWeek(10, this.selectedProduceTypeIndex).subscribe({
       next: (data) => {
         this.produceThisWeek = data;
+        console.log(this.produceThisWeek);
+        if (this.produceThisWeek) {
+          this.produce = this.produceThisWeek[0].name
+          this.imageUrl = this.produceThisWeek[0].imageUrl;
+          this.fontColour = this.produceThisWeek[0].colour;
+          this.weeksRemaining = this.produceThisWeek[0].weeksRemaining;
+        }
       },
       error: (error) => {
         this.errorMessage = 'Error fetching data';
         console.error(error);
       }
     });
-    console.log(this.produceThisWeek);
   }
+
 
   onDropdownChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
